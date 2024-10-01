2a4cc07e575c18d2ed40f797ea1386ea132b0b2f
prgrmz07 <prgrmz07@163.com>
2024-10-01 12:04:28 +0800

    优化整理1

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ```shell
-F_debFs=/app4/debianfs-arm64-full D_work=/app4/eadb_work_home/ phone=24846224 bash -x <(curl https://gitee.com/imagg/tiann--eadb/raw/z/main/main.sh)
+F_debFs=/tmp/debianfs-arm64-full
+#github通常需要科学上网，可手工下载后保存为该文件名
+wget https://github.com/tiann/eadb/releases/download/v0.1.6/debianfs-arm64-full.tar.gz -O $F_debFs.tar.gz
+
+F_debFs=$F_debFs D_work=/app4/eadb_work_home/ phone=24846224 bash <(curl https://gitee.com/imagg/tiann--eadb/raw/z/main/main.sh)
+#若排查错误， 请修改 "bash" 为 "bash -x"
 ```
--- assets/basic_setup.sh
+++ assets/basic_setup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+set -e -u
+
+ls -lh /etc/apt/sources.list
+
 apt update
 apt install -y python3-setuptools
 apt install -y zip
 apt install -y htop
+apt install -y build-essential
 
 apt install -y openssh-server
 /etc/init.d/ssh restart
+
+echo "已安装: python3-setuptools, zip, htop, build-essential, openssh-server"
+
 adduser z
-ifconfig wlan0 #10.0.4.107
-echo ssh z@10.0.4.107
+
+ip_addr=$(ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+echo "添加用户z, 无线网卡wlan的ip为${ip_addr}, 连接我命令[ssh z@${ip_addr}]"
